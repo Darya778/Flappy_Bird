07473ac4fc584facb15ad8d23a737a7838bbca0f
Darya <darya.n.2004@mail.ru>
2025-10-27 15:20:15 +0800

    background fixed

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,19 +1,18 @@
 extends Node2D
 
-@export var bg_speed := 20.0      # фон движется медленно
-@export var floor_speed := 120.0  # земля движется быстрее
+@export var bg_speed := 20.0
+@export var floor_speed := 120.0
 
-# массивы спрайтов
-var bg_sprites := []   # сюда добавим bg_1, bg_2, bg_3
-var floor_sprites := [] # сюда добавим flo_1, flo_2, flo_3
+var bg_sprites := []
+var floor_sprites := []
 
 var bg_width: float
 var floor_width: float
 var game_started: bool = false
 
 func _ready():
-	bg_sprites = [$bg_1, $bg_2, $bg_3]
-	floor_sprites = [$flo_1, $flo_2, $flo_3]
+	bg_sprites = [get_node("bg_1"), get_node("bg_2"), get_node("bg_3")]
+	floor_sprites = [get_node("flo_1"), get_node("flo_2"), get_node("flo_3")]
 
 	bg_width = bg_sprites[0].texture.get_size().x * bg_sprites[0].scale.x
 	floor_width = floor_sprites[0].texture.get_size().x * floor_sprites[0].scale.x
@@ -23,7 +22,10 @@ func _ready():
 		floor_sprites[i].position.x = floor_width * i
 
 func _process(delta):
-	
+	if not game_started:
+		if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			game_started = true
+		return
 		
 	_scroll_layer(bg_sprites, bg_speed, delta)
 	_scroll_layer(floor_sprites, floor_speed, delta)
